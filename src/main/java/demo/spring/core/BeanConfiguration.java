abcd2c515781edf5f75bb627a6a6d22886713add
Isma Turrofiah~ <73883487+Ismaturrofiah@users.noreply.github.com>
2024-02-03 22:04:42 +0700

    bean and duplicate test

---
+++ src/main/java/demo/spring/core/BeanConfiguration.java
@@ -0,0 +1,19 @@
+package demo.spring.core;
+
+import demo.spring.core.data.Foo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class BeanConfiguration {
+
+    @Bean
+    public Foo foo() {
+        Foo foo = new Foo();
+        log.info("Create new foo");
+        return foo;
+    }
+
+}
